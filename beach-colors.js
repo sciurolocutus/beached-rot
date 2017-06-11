5d95717ff6d073c0ff2775b0c8471d6716011880
sciurolocutus <Robert.Wood.Busby@gmail.com>
2017-06-10 22:13:25 -0500

    added new random beach sand function

--- beach-colors.js
+++ beach-colors.js
@@ -1,4 +1,13 @@
 var Colors = {
   'palesand' = 'e9d4b7',
-  'light-peach-sand' = 'ffdcba'
+  'light-peach-sand' = 'ffdcba',
+  'random-beachsand' = function() {
+    var randomColorChannel = function (min, max) {
+      return Math.floor((Math.random() * (max-min) + min)).toString(16);
+    }
+    var r = randomColorChannel(234, 256);
+    var g = randomColorChannel(212, 220);
+    var b = randomColorChannel(183, 186);
+    return [r, g, b].join('');
+  }
 }
